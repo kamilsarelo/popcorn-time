7eaad78c6ff1a1b72295c2dd8e8562a7fb6a85b0
kamilsarelo <kamil.sarelo@gmail.com>
2019-03-20 20:43:00 +0100

    year and rating vars

--- filter.js
+++ filter.js
@@ -1,17 +1,19 @@
 // use F12 or CMD+0 to open JS console in Popcorn-Time
 
 // start filter
+var yearMin = 2018;
+var ratingMin = 6;
 var timer = setInterval(function() {
 	var nodeList = document.querySelectorAll("#content > div > div.list-region > ul > div.items > li");
 	nodeList.forEach(function(node) {
 		if (node != null) { // equivalent to: typeof(node) !== "undefined" && node !== null
 			var year = node.querySelector("p.year");
 			var rating = node.querySelector("div.rating-value");
-			if (year != null && year.innerText < 2018) {
+			if (year != null && year.innerText < yearMin) {
 				if (node.parentNode != null) {
 					node.parentNode.removeChild(node);
 				}
-			} else if (rating != null && rating.innerText[0] < 6) {
+			} else if (rating != null && rating.innerText[0] < ratingMin) {
 				if (node.parentNode != null) {
 					node.parentNode.removeChild(node);
 				}
